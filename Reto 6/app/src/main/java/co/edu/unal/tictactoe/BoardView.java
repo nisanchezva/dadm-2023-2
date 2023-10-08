97ea0c914ed6f420382f81a27e2737bd668b919e
Nicolas Sanchez Vargas <nisanchezva@unal.edu.co>
2023-10-08 14:54:39 -0500

    Reto 6

---
+++ Reto 6/app/src/main/java/co/edu/unal/tictactoe/BoardView.java
@@ -0,0 +1,101 @@
+package co.edu.unal.tictactoe;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoardView extends View {
+
+    // Width of the board grid lines
+    public static final int GRID_WIDTH = 6;
+    private Bitmap mHumanBitmap;
+    private Bitmap mComputerBitmap;
+    private Paint mPaint;
+    private TicTacToeGame mGame;
+
+
+    public void setGame(TicTacToeGame game) {
+        mGame = game;
+    }
+
+    public void initialize() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mHumanBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_cancel);
+        mComputerBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_playback_stop);
+    }
+
+    public BoardView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public BoardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initialize();
+    }
+
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public int getBoardCellWidth() {
+        return getWidth() / 3;
+    }
+    public int getBoardCellHeight() {
+        return getHeight() / 3;
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+// Determine the width and height of the View
+        int boardWidth = getWidth();
+        int boardHeight = getHeight();
+// Make thick, light gray lines
+        mPaint.setColor(Color.LTGRAY);
+        mPaint.setStrokeWidth(GRID_WIDTH);
+// Draw the two vertical board lines
+        int cellWidth = boardWidth / 3;
+        canvas.drawLine(cellWidth, 0, cellWidth, boardHeight, mPaint);
+        canvas.drawLine(cellWidth * 2, 0, cellWidth * 2, boardHeight, mPaint);
+        // Draw the two horizontal board lines
+        int cellHeight = boardHeight / 3;
+        canvas.drawLine(0, cellHeight, boardWidth, cellHeight, mPaint);
+        canvas.drawLine(0, cellHeight * 2, boardWidth, cellHeight * 2, mPaint);
+
+        // Draw all the X and squares images
+        for (int i = 0; i < 9; i++) {
+            int col = i % 3;
+            int row = i / 3;
+// Define the boundaries of a destination rectangle for the image
+            int left = col * cellWidth;
+            int top = row * cellHeight;
+            int right = (col + 1) * cellWidth;
+            int bottom = (row + 1) * cellHeight;
+            Rect rectangle = new Rect(left,top,right,bottom);
+
+            if (mGame != null && mGame.getBoardOccupant(i) == TicTacToeGame.HUMAN_PLAYER) {
+                canvas.drawBitmap(mHumanBitmap,
+                        null, // src
+                        rectangle, // dest
+                        null);
+
+            } else if (mGame != null && mGame.getBoardOccupant(i) == TicTacToeGame.COMPUTER_PLAYER) {
+                canvas.drawBitmap(mComputerBitmap,
+                        null, // src
+                        rectangle, // dest
+                        null);
+            }
+        }
+    }
+}
+
+
+
